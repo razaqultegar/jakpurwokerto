8f6e99eb7dda6b025080309378421a5f56358d94
razaqultegar <myrazaqul@gmail.com>
2020-03-04 13:35:37 +0700

    Fix database

--- database/db_jakpurwokerto_kosong.sql
+++ database/db_jakpurwokerto_kosong.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 14 Feb 2020 pada 19.01
+-- Waktu pembuatan: 04 Mar 2020 pada 07.29
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 5.6.40
 
@@ -38,8 +38,7 @@ CREATE TABLE `default_token` (
 --
 
 INSERT INTO `default_token` (`tokenId`, `token`) VALUES
-(1, 'cobatoken'),
-(2, 'p8>)DU)VCX');
+(1, 'cobatoken');
 
 -- --------------------------------------------------------
 
@@ -60,7 +59,7 @@ CREATE TABLE `default_user` (
 --
 
 INSERT INTO `default_user` (`userId`, `realname`, `username`, `password`, `foto`) VALUES
-(1, 'Super Admin', 'superadmin', '17c4520f6cfd1ab53d8745e84681eb49', 'default.jpg');
+(1, 'Super Admin', 'superadmin', 'd389d5247e4f47ecf0a33dead2428ec0', NULL);
 
 -- --------------------------------------------------------
 
@@ -69,14 +68,15 @@ INSERT INTO `default_user` (`userId`, `realname`, `username`, `password`, `foto`
 --
 
 CREATE TABLE `jakpwt_anggota` (
-  `noKta` varchar(100) NOT NULL,
+  `agtId` int(11) NOT NULL,
+  `agtNoKta` varchar(100) DEFAULT NULL,
   `agtIdWilayah` int(20) DEFAULT NULL,
   `agtNama` varchar(250) DEFAULT NULL,
   `agtNmPendek` varchar(20) DEFAULT NULL,
   `agtTmptLahir` varchar(500) DEFAULT NULL,
   `agtTglLahir` date DEFAULT NULL,
-  `agtJnsKelamin` enum('L','P','N') DEFAULT NULL,
   `agtUmur` int(20) DEFAULT NULL,
+  `agtJnsKelamin` enum('L','P','N') DEFAULT NULL,
   `agtIdPendidikan` int(20) DEFAULT NULL,
   `agtIdPekerjaan` int(20) DEFAULT NULL,
   `agtKelurahan` varchar(250) DEFAULT NULL,
@@ -85,8 +85,9 @@ CREATE TABLE `jakpwt_anggota` (
   `agtKdPos` varchar(5) DEFAULT NULL,
   `agtNoTelp` varchar(13) DEFAULT NULL,
   `agtEmail` varchar(250) DEFAULT NULL,
-  `agtUkrnKaos` enum('S','M','L','XL','XXL','XXXL') DEFAULT NULL,
-  `agtFoto` varchar(100) NOT NULL DEFAULT 'default.jpg',
+  `agtUkrnKaos` varchar(20) DEFAULT NULL,
+  `agtFoto` varchar(100) DEFAULT NULL,
+  `agtStatusKta` enum('0','1') NOT NULL DEFAULT '0',
   `agtBrlkDari` varchar(100) DEFAULT NULL,
   `agtBrlkSampai` varchar(100) DEFAULT NULL,
   `agtTglInsert` date DEFAULT NULL,
@@ -114,30 +115,6 @@ CREATE TABLE `jakpwt_kas` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jakpwt_ref_agama`
---
-
-CREATE TABLE `jakpwt_ref_agama` (
-  `agmIdAgama` int(20) NOT NULL,
-  `agmNama` varchar(500) DEFAULT NULL,
-  `agmLastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data untuk tabel `jakpwt_ref_agama`
---
-
-INSERT INTO `jakpwt_ref_agama` (`agmIdAgama`, `agmNama`, `agmLastUpdate`) VALUES
-(1, 'Islam', '2020-02-14 17:46:29'),
-(2, 'Kristen', '2020-02-14 17:47:08'),
-(3, 'Katholik', '2020-02-14 17:47:46'),
-(4, 'Hindu', '2020-02-14 17:48:01'),
-(5, 'Budha', '2020-02-14 17:48:12'),
-(6, 'Konghucu', '2020-02-14 17:48:26');
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `jakpwt_ref_pekerjaan`
 --
 
@@ -152,98 +129,98 @@ CREATE TABLE `jakpwt_ref_pekerjaan` (
 --
 
 INSERT INTO `jakpwt_ref_pekerjaan` (`pkjIdPekerjaan`, `pkjNama`, `pkjLastUpdate`) VALUES
-(1, 'Petani / Pekebun', '2018-06-06 11:57:09'),
-(2, 'Tentara Nasional Indonesia', '2018-06-02 05:37:27'),
-(4, 'Pegawai Negeri Sipil', '2018-06-02 05:19:50'),
-(5, 'Karyawan Swasta', '2017-09-12 07:49:49'),
-(6, 'Wirausaha', '2017-09-12 07:50:00'),
-(7, 'Buruh Migran (TKI)', '2017-09-12 07:50:15'),
-(8, 'Pedagang', '2017-09-12 07:50:24'),
-(9, 'Buruh Harian Lepas', '2017-09-18 14:41:59'),
-(11, 'Guru', '2017-09-12 07:51:10'),
-(12, 'Kepala Desa', '2017-09-12 07:51:36'),
-(13, 'Perangkat Desa', '2017-09-12 07:51:44'),
-(14, 'Bupati', '2017-09-12 07:51:53'),
-(15, 'Anggota DPRD Kabupaten / Kota', '2018-06-06 11:43:16'),
-(17, 'Anggota DPR RI', '2017-09-12 07:52:28'),
-(18, 'Anggota DPD', '2018-06-06 11:42:44'),
-(19, 'Anggota DPRD Provinsi', '2017-09-12 07:52:51'),
-(20, 'Mengurus Rumah Tangga', '2017-09-12 07:53:16'),
-(21, 'Belum/ Tidak Bekerja', '2017-09-18 14:23:51'),
-(22, 'Pelajar/ Mahasiswa', '2017-09-18 14:24:15'),
-(24, 'Dokter', '2017-09-12 07:54:00'),
-(25, 'Pengacara', '2017-09-12 07:54:09'),
-(26, 'Menteri', '2017-09-12 07:54:23'),
-(27, 'Presiden', '2017-09-12 07:54:31'),
-(29, 'Kepolisian RI', '2018-06-02 05:47:03'),
-(30, 'Pensiunan', '2017-09-18 14:25:09'),
-(31, 'Perdagangan', '2017-09-18 14:25:38'),
-(32, 'Peternak', '2017-09-18 14:27:05'),
-(33, 'Nelayan/ Perikanan', '2017-09-18 14:34:09'),
-(35, 'Industri', '2017-09-18 14:39:09'),
-(36, 'Konstruksi', '2017-09-18 14:39:35'),
-(37, 'Transportasi', '2017-09-18 14:40:31'),
-(38, 'Karyawan BUMN', '2017-09-18 14:40:58'),
-(39, 'Karyawan BUMD', '2017-09-18 14:41:11'),
-(41, 'Buruh Tani / Perkebunan', '2018-06-06 11:46:34'),
-(42, 'Buruh Nelayan / Perikanan', '2018-06-06 11:46:14'),
-(43, 'Buruh Peternakan', '2017-09-18 14:43:05'),
-(44, 'Pembantu Rumah Tangga', '2017-09-18 14:43:25'),
-(45, 'Tukang Cukur', '2017-09-18 14:43:40'),
-(46, 'Tukang Listrik', '2017-09-18 14:43:57'),
-(47, 'Tukang Batu', '2017-09-18 14:44:06'),
-(48, 'Tukang Kayu', '2017-09-18 14:44:18'),
-(49, 'Tukang Sol Sepatu', '2017-09-18 14:44:29'),
-(50, 'Tukang Las / Pandai Besi', '2018-06-06 12:02:33'),
-(51, 'Tukang Jahit', '2017-09-18 14:44:57'),
-(52, 'Tukang Gigi', '2017-09-18 14:46:46'),
-(53, 'Penata Rias', '2017-09-18 14:48:36'),
-(54, 'Penata Busana', '2017-09-18 14:48:50'),
-(55, 'Penata Rambut', '2017-09-18 14:48:57'),
-(56, 'Mekanik', '2017-09-18 14:49:07'),
-(57, 'Seniman', '2017-09-18 14:49:13'),
-(58, 'Tabib', '2017-09-18 14:49:26'),
-(59, 'Paraji', '2017-09-18 14:49:31'),
-(60, 'Perancang Busana', '2017-09-18 14:49:48'),
-(61, 'Penterjemah', '2018-06-06 11:55:35'),
-(62, 'Imam Masjid', '2017-09-18 14:50:16'),
-(63, 'Pendeta', '2017-09-18 14:50:28'),
-(64, 'Pastur', '2018-06-06 11:53:11'),
-(65, 'Wartawan', '2017-09-18 14:50:52'),
-(66, 'Ustadz / Mubaligh', '2018-06-06 12:02:56'),
-(67, 'Juru Masak', '2017-09-18 14:51:24'),
-(68, 'Promotor Acara', '2017-09-18 14:51:43'),
-(69, 'Anggota BPK', '2017-09-18 14:52:06'),
-(70, 'Anggota Mahkamah Konstitusi', '2017-09-18 14:52:29'),
-(71, 'Dosen', '2017-09-18 14:53:49'),
-(72, 'Pilot', '2017-09-18 14:54:04'),
-(73, 'Notaris', '2017-09-18 14:54:50'),
-(74, 'Arsitek', '2017-09-18 14:55:18'),
-(75, 'Akuntan', '2017-09-18 14:55:30'),
-(76, 'Konsultan', '2017-09-18 14:55:52'),
-(77, 'Perawat', '2017-09-18 14:56:17'),
-(78, 'Apoteker', '2017-09-18 14:56:39'),
-(79, 'Psikiater / Psikolog', '2018-06-06 12:01:21'),
-(80, 'Penyiar Televisi', '2017-09-18 14:58:32'),
-(81, 'Penyiar Radio', '2017-09-18 14:58:39'),
-(82, 'Pelaut', '2017-09-18 14:58:57'),
-(83, 'Peneliti', '2017-09-18 14:59:04'),
-(84, 'Sopir', '2017-09-18 14:59:20'),
-(85, 'Pialang', '2017-09-18 14:59:52'),
-(86, 'Paranormal', '2017-09-18 15:00:07'),
-(87, 'Biarawati', '2018-06-06 11:45:41'),
-(88, 'Wiraswasta', '2017-09-18 15:01:07'),
-(89, 'Pekerjaan Lainnya', '2017-09-18 15:01:21'),
-(90, 'Duta Besar', '2017-09-18 15:01:40'),
-(91, 'Karyawan Honorer', '2017-11-15 04:18:54'),
-(92, 'Bidan', '2017-11-24 04:22:57'),
-(93, 'Anggota Kabinet / Kementerian', '2018-06-06 11:45:08'),
-(94, 'Gubernur', '2018-06-06 11:46:57'),
-(95, 'Wakil Bupati', '2018-06-06 12:05:39'),
-(96, 'Wakil Gubernur', '2018-06-06 12:05:56'),
-(97, 'Wakil Presiden', '2018-06-06 12:06:15'),
-(98, 'Wakil Walikota', '2018-06-06 12:06:39'),
-(99, 'Walikota', '2018-06-06 12:06:54');
+(1, 'Petani / Pekebun', '2020-02-17 07:07:14'),
+(2, 'Tentara Nasional Indonesia', '2020-02-17 07:07:16'),
+(3, 'Pegawai Negeri Sipil', '2020-02-17 07:07:18'),
+(4, 'Karyawan Swasta', '2020-02-17 07:07:21'),
+(5, 'Wirausaha', '2020-02-17 07:07:25'),
+(6, 'Buruh Migran (TKI)', '2020-02-17 07:07:35'),
+(7, 'Pedagang', '2020-02-17 07:07:38'),
+(8, 'Buruh Harian Lepas', '2020-02-17 07:07:42'),
+(9, 'Guru', '2020-02-17 07:11:46'),
+(10, 'Kepala Desa', '2020-02-17 07:12:15'),
+(11, 'Perangkat Desa', '2020-02-17 07:12:15'),
+(12, 'Bupati', '2020-02-17 07:12:15'),
+(13, 'Anggota DPRD Kabupaten / Kota', '2020-02-17 07:12:15'),
+(14, 'Anggota DPR RI', '2020-02-17 07:12:16'),
+(15, 'Anggota DPD', '2020-02-17 07:12:16'),
+(16, 'Anggota DPRD Provinsi', '2020-02-17 07:12:16'),
+(17, 'Mengurus Rumah Tangga', '2020-02-17 07:12:16'),
+(18, 'Belum/ Tidak Bekerja', '2020-02-17 07:12:16'),
+(19, 'Pelajar/ Mahasiswa', '2020-02-17 07:12:16'),
+(20, 'Dokter', '2020-02-17 07:12:16'),
+(21, 'Pengacara', '2020-02-17 07:12:16'),
+(22, 'Menteri', '2020-02-17 07:12:16'),
+(23, 'Presiden', '2020-02-17 07:12:16'),
+(24, 'Kepolisian RI', '2020-02-17 07:12:16'),
+(25, 'Pensiunan', '2020-02-17 07:12:16'),
+(26, 'Perdagangan', '2020-02-17 07:12:16'),
+(27, 'Peternak', '2020-02-17 07:12:16'),
+(28, 'Nelayan/ Perikanan', '2020-02-17 07:12:16'),
+(29, 'Industri', '2020-02-17 07:12:17'),
+(30, 'Konstruksi', '2020-02-17 07:12:17'),
+(31, 'Transportasi', '2020-02-17 07:12:17'),
+(32, 'Karyawan BUMN', '2020-02-17 07:12:17'),
+(33, 'Karyawan BUMD', '2020-02-17 07:12:17'),
+(34, 'Buruh Tani / Perkebunan', '2020-02-17 07:12:17'),
+(35, 'Buruh Nelayan / Perikanan', '2020-02-17 07:12:17'),
+(36, 'Buruh Peternakan', '2020-02-17 07:12:17'),
+(37, 'Pembantu Rumah Tangga', '2020-02-17 07:12:17'),
+(38, 'Tukang Cukur', '2020-02-17 07:12:17'),
+(39, 'Tukang Listrik', '2020-02-17 07:12:17'),
+(40, 'Tukang Batu', '2020-02-17 07:12:17'),
+(41, 'Tukang Kayu', '2020-02-17 07:12:17'),
+(42, 'Tukang Sol Sepatu', '2020-02-17 07:12:18'),
+(43, 'Tukang Las / Pandai Besi', '2020-02-17 07:12:18'),
+(44, 'Tukang Jahit', '2020-02-17 07:12:18'),
+(45, 'Tukang Gigi', '2020-02-17 07:12:18'),
+(46, 'Penata Rias', '2020-02-17 07:12:18'),
+(47, 'Penata Busana', '2020-02-17 07:12:18'),
+(48, 'Penata Rambut', '2020-02-17 07:12:19'),
+(49, 'Mekanik', '2020-02-17 07:12:19'),
+(50, 'Seniman', '2020-02-17 07:12:19'),
+(51, 'Tabib', '2020-02-17 07:16:38'),
+(52, 'Paraji', '2020-02-17 07:16:38'),
+(53, 'Perancang Busana', '2020-02-17 07:16:38'),
+(54, 'Penterjemah', '2020-02-17 07:16:38'),
+(55, 'Imam Masjid', '2020-02-17 07:16:38'),
+(56, 'Pendeta', '2020-02-17 07:16:38'),
+(57, 'Pastur', '2020-02-17 07:16:38'),
+(58, 'Wartawan', '2020-02-17 07:16:38'),
+(59, 'Ustadz / Mubaligh', '2020-02-17 07:16:39'),
+(60, 'Juru Masak', '2020-02-17 07:16:39'),
+(61, 'Promotor Acara', '2020-02-17 07:16:39'),
+(62, 'Anggota BPK', '2020-02-17 07:16:39'),
+(63, 'Anggota Mahkamah Konstitusi', '2020-02-17 07:16:39'),
+(64, 'Dosen', '2020-02-17 07:16:39'),
+(65, 'Pilot', '2020-02-17 07:16:39'),
+(66, 'Notaris', '2020-02-17 07:16:39'),
+(67, 'Arsitek', '2020-02-17 07:16:39'),
+(68, 'Akuntan', '2020-02-17 07:16:40'),
+(69, 'Konsultan', '2020-02-17 07:16:40'),
+(70, 'Perawat', '2020-02-17 07:16:40'),
+(71, 'Apoteker', '2020-02-17 07:16:40'),
+(72, 'Psikiater / Psikolog', '2020-02-17 07:16:40'),
+(73, 'Penyiar Televisi', '2020-02-17 07:16:40'),
+(74, 'Penyiar Radio', '2020-02-17 07:16:40'),
+(75, 'Pelaut', '2020-02-17 07:16:40'),
+(76, 'Peneliti', '2020-02-17 07:16:40'),
+(77, 'Sopir', '2020-02-17 07:16:40'),
+(78, 'Pialang', '2020-02-17 07:16:40'),
+(79, 'Paranormal', '2020-02-17 07:16:40'),
+(80, 'Biarawati', '2020-02-17 07:16:41'),
+(81, 'Wiraswasta', '2020-02-17 07:16:41'),
+(82, 'Pekerjaan Lainnya', '2020-02-17 07:16:41'),
+(83, 'Duta Besar', '2020-02-17 07:16:41'),
+(84, 'Karyawan Honorer', '2020-02-17 07:16:41'),
+(85, 'Bidan', '2020-02-17 07:16:41'),
+(86, 'Anggota Kabinet / Kementerian', '2020-02-17 07:16:41'),
+(87, 'Gubernur', '2020-02-17 07:16:41'),
+(88, 'Wakil Bupati', '2020-02-17 07:16:42'),
+(89, 'Wakil Gubernur', '2020-02-17 07:16:42'),
+(90, 'Wakil Presiden', '2020-02-17 07:16:42'),
+(91, 'Wakil Walikota', '2020-02-17 07:16:42'),
+(92, 'Walikota', '2020-02-17 07:16:42');
 
 -- --------------------------------------------------------
 
@@ -252,7 +229,7 @@ INSERT INTO `jakpwt_ref_pekerjaan` (`pkjIdPekerjaan`, `pkjNama`, `pkjLastUpdate`
 --
 
 CREATE TABLE `jakpwt_ref_pendidikan` (
-  `dikIdPendidikan` int(20) NOT NULL,
+  `dikIdPendidikan` int(11) NOT NULL,
   `dikPendidikan` varchar(500) DEFAULT NULL,
   `dikLastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -262,16 +239,39 @@ CREATE TABLE `jakpwt_ref_pendidikan` (
 --
 
 INSERT INTO `jakpwt_ref_pendidikan` (`dikIdPendidikan`, `dikPendidikan`, `dikLastUpdate`) VALUES
-(2, 'Tidak / Belum Sekolah', '2019-01-19 18:38:52'),
-(3, 'Tamat SD / Sederajat', '2018-06-06 12:14:58'),
-(4, 'SLTP / Sederajat', '2017-10-01 14:57:05'),
-(5, 'SLTA / Sederajat', '2017-10-01 14:57:28'),
-(6, 'Diploma I / II', '2017-10-01 14:57:59'),
-(9, 'Diploma IV / Strata I\r\n', '2017-09-30 20:24:22'),
-(10, 'Belum Tamat SD / Sederajat\r\n', '2017-09-30 20:24:44'),
-(11, 'Akademi / Diploma III / Sarjana Muda', '2017-10-01 15:17:03'),
-(12, 'Strata II', '2017-10-01 15:16:34'),
-(13, 'Strata III', '2017-10-01 15:16:40');
+(1, 'Tidak / Belum Sekolah', '2020-02-17 07:06:28'),
+(2, 'Tamat SD / Sederajat', '2020-02-17 07:06:32'),
+(3, 'SLTP / Sederajat', '2020-02-17 07:06:36'),
+(4, 'SLTA / Sederajat', '2020-02-17 07:06:40'),
+(5, 'Diploma I / II', '2020-02-17 07:06:44'),
+(6, 'Diploma IV / Strata I\r\n', '2020-02-17 07:06:47'),
+(7, 'Belum Tamat SD / Sederajat\r\n', '2020-02-17 07:06:51'),
+(8, 'Akademi / Diploma III / Sarjana Muda', '2020-02-17 07:06:55'),
+(9, 'Strata II', '2020-02-17 07:06:58'),
+(10, 'Strata III', '2020-02-17 07:07:01');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jakpwt_ref_wilayah`
+--
+
+CREATE TABLE `jakpwt_ref_wilayah` (
+  `wilIdWilayah` int(11) NOT NULL,
+  `wilNama` varchar(500) DEFAULT NULL,
+  `wilLastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `jakpwt_ref_wilayah`
+--
+
+INSERT INTO `jakpwt_ref_wilayah` (`wilIdWilayah`, `wilNama`, `wilLastUpdate`) VALUES
+(1, 'Ajibarang', '2020-02-27 11:06:39'),
+(2, 'Banyumas', '2020-02-27 11:06:54'),
+(3, 'Purwokerto', '2020-02-27 13:20:52'),
+(4, 'Rantau', '2020-02-27 13:20:55'),
+(5, 'Wangon', '2020-02-27 13:21:03');
 
 --
 -- Indexes for dumped tables
@@ -293,19 +293,16 @@ ALTER TABLE `default_user`
 -- Indeks untuk tabel `jakpwt_anggota`
 --
 ALTER TABLE `jakpwt_anggota`
-  ADD PRIMARY KEY (`noKta`);
+  ADD PRIMARY KEY (`agtId`),
+  ADD KEY `agtIdWilayah` (`agtIdWilayah`),
+  ADD KEY `agtIdPendidikan` (`agtIdPendidikan`),
+  ADD KEY `agtIdPekerjaan` (`agtIdPekerjaan`);
 
 --
 -- Indeks untuk tabel `jakpwt_kas`
 --
 ALTER TABLE `jakpwt_kas`
   ADD PRIMARY KEY (`kasId`);
-
---
--- Indeks untuk tabel `jakpwt_ref_agama`
---
-ALTER TABLE `jakpwt_ref_agama`
-  ADD PRIMARY KEY (`agmIdAgama`);
 
 --
 -- Indeks untuk tabel `jakpwt_ref_pekerjaan`
@@ -317,7 +314,13 @@ ALTER TABLE `jakpwt_ref_pekerjaan`
 -- Indeks untuk tabel `jakpwt_ref_pendidikan`
 --
 ALTER TABLE `jakpwt_ref_pendidikan`
-  ADD PRIMARY KEY (`dikIdPendidikan`,`dikLastUpdate`);
+  ADD PRIMARY KEY (`dikIdPendidikan`) USING BTREE;
+
+--
+-- Indeks untuk tabel `jakpwt_ref_wilayah`
+--
+ALTER TABLE `jakpwt_ref_wilayah`
+  ADD PRIMARY KEY (`wilIdWilayah`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -327,13 +330,25 @@ ALTER TABLE `jakpwt_ref_pendidikan`
 -- AUTO_INCREMENT untuk tabel `default_token`
 --
 ALTER TABLE `default_token`
-  MODIFY `tokenId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `tokenId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `default_user`
 --
 ALTER TABLE `default_user`
   MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `jakpwt_anggota`
+--
+ALTER TABLE `jakpwt_anggota`
+  MODIFY `agtId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `jakpwt_kas`
+--
+ALTER TABLE `jakpwt_kas`
+  MODIFY `kasId` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
