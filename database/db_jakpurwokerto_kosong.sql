d44e27a069781cd744b268cc22c5e3c657e933e1
razaqultegar <myrazaqul@gmail.com>
2025-12-06 07:12:39 +0700

    docs: update databases

--- database/db_jakpurwokerto_kosong.sql
+++ database/db_jakpurwokerto_kosong.sql
@@ -1,355 +1,108 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
+-- -------------------------------------------------------------
+-- TablePlus 6.7.4(642)
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 04 Mar 2020 pada 14.21
--- Versi server: 10.1.38-MariaDB
--- Versi PHP: 5.6.40
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- https://tableplus.com/
+--
+-- Database: jakpurwokerto
+-- Generation Time: 2025-12-06 02:54:51.9270
+-- -------------------------------------------------------------
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Database: `db_jakpurwokerto`
---
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `default_token`
---
-
+DROP TABLE IF EXISTS `default_token`;
 CREATE TABLE `default_token` (
-  `tokenId` int(11) NOT NULL,
-  `token` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `tokenId` int NOT NULL AUTO_INCREMENT,
+  `token` varchar(10) NOT NULL,
+  PRIMARY KEY (`tokenId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `default_user`
---
-
+DROP TABLE IF EXISTS `default_user`;
 CREATE TABLE `default_user` (
-  `userId` int(11) NOT NULL,
+  `userId` int NOT NULL AUTO_INCREMENT,
   `realname` varchar(100) DEFAULT NULL,
   `username` varchar(32) DEFAULT NULL,
   `password` varchar(32) DEFAULT NULL,
-  `foto` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `foto` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 
---
--- Dumping data untuk tabel `default_user`
---
-
-INSERT INTO `default_user` (`userId`, `realname`, `username`, `password`, `foto`) VALUES
-(1, 'Super Admin', 'superadmin', 'd389d5247e4f47ecf0a33dead2428ec0', NULL);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `jakpwt_anggota`
---
-
+DROP TABLE IF EXISTS `jakpwt_anggota`;
 CREATE TABLE `jakpwt_anggota` (
-  `agtId` int(11) NOT NULL,
-  `agtNoKta` varchar(100) DEFAULT NULL,
-  `agtIdWilayah` int(20) DEFAULT NULL,
-  `agtNama` varchar(250) DEFAULT NULL,
-  `agtNmPendek` varchar(20) DEFAULT NULL,
-  `agtTmptLahir` varchar(500) DEFAULT NULL,
-  `agtTglLahir` date DEFAULT NULL,
-  `agtUmur` int(20) DEFAULT NULL,
-  `agtJnsKelamin` enum('L','P','N') DEFAULT NULL,
-  `agtIdPendidikan` int(20) DEFAULT NULL,
-  `agtIdPekerjaan` int(20) DEFAULT NULL,
-  `agtKelurahan` varchar(250) DEFAULT NULL,
-  `agtKecamatan` varchar(250) DEFAULT NULL,
-  `agtAlamatJalan` text,
-  `agtKdPos` varchar(5) DEFAULT NULL,
-  `agtNoTelp` varchar(13) DEFAULT NULL,
-  `agtEmail` varchar(250) DEFAULT NULL,
-  `agtUkrnKaos` varchar(20) DEFAULT NULL,
-  `agtFoto` varchar(100) DEFAULT NULL,
-  `agtStatusKta` enum('0','1') NOT NULL DEFAULT '0',
-  `agtBrlkDari` varchar(100) DEFAULT NULL,
-  `agtBrlkSampai` varchar(100) DEFAULT NULL,
-  `agtTglInsert` date DEFAULT NULL,
-  `agtLastUpdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `agtId` int NOT NULL AUTO_INCREMENT,
+  `agtIdWilayah` bigint unsigned DEFAULT NULL,
+  `agtFoto` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `agtFotoKTA` varchar(100) DEFAULT NULL,
+  `agtNik` varchar(16) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `agtNoKTA` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `agtNama` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `agtJnsKelamin` enum('L','P') DEFAULT 'L',
+  `agtTmptLahir` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `agtTglLahir` date NOT NULL,
+  `agtUmur` varchar(2) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `agtProvinsi` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `agtKabupaten` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `agtKecamatan` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `agtKelurahan` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `agtAlamatJalan` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `agtEmail` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `agtNoTelp` varchar(13) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `agtUkuranKaos` enum('S','M','L','XL','XXL') DEFAULT 'S',
+  `agtMetodePembayaran` enum('BCA','MANDIRI') NOT NULL DEFAULT 'BCA',
+  `agtTglInsert` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `agtLastUpdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`agtId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `jakpwt_kas`
---
-
+DROP TABLE IF EXISTS `jakpwt_kas`;
 CREATE TABLE `jakpwt_kas` (
-  `kasId` int(11) NOT NULL,
-  `kasWilId` int(20) DEFAULT NULL,
+  `kasId` int NOT NULL AUTO_INCREMENT,
+  `kasWilId` int DEFAULT NULL,
   `kasTanggal` date DEFAULT NULL,
   `kasMasukUraian` text,
   `kasMasuk` varchar(100) DEFAULT NULL,
   `kasKeluarUraian` text,
   `kasKeluar` varchar(100) DEFAULT NULL,
   `kasSaldo` varchar(100) DEFAULT NULL,
-  `kasLastUpdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `kasLastUpdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`kasId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `jakpwt_ref_pekerjaan`
---
-
-CREATE TABLE `jakpwt_ref_pekerjaan` (
-  `pkjIdPekerjaan` int(11) NOT NULL,
-  `pkjNama` varchar(500) DEFAULT NULL,
-  `pkjLastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data untuk tabel `jakpwt_ref_pekerjaan`
---
-
-INSERT INTO `jakpwt_ref_pekerjaan` (`pkjIdPekerjaan`, `pkjNama`, `pkjLastUpdate`) VALUES
-(1, 'Petani / Pekebun', '2020-02-17 07:07:14'),
-(2, 'Tentara Nasional Indonesia', '2020-02-17 07:07:16'),
-(3, 'Pegawai Negeri Sipil', '2020-02-17 07:07:18'),
-(4, 'Karyawan Swasta', '2020-02-17 07:07:21'),
-(5, 'Wirausaha', '2020-02-17 07:07:25'),
-(6, 'Buruh Migran (TKI)', '2020-02-17 07:07:35'),
-(7, 'Pedagang', '2020-02-17 07:07:38'),
-(8, 'Buruh Harian Lepas', '2020-02-17 07:07:42'),
-(9, 'Guru', '2020-02-17 07:11:46'),
-(10, 'Kepala Desa', '2020-02-17 07:12:15'),
-(11, 'Perangkat Desa', '2020-02-17 07:12:15'),
-(12, 'Bupati', '2020-02-17 07:12:15'),
-(13, 'Anggota DPRD Kabupaten / Kota', '2020-02-17 07:12:15'),
-(14, 'Anggota DPR RI', '2020-02-17 07:12:16'),
-(15, 'Anggota DPD', '2020-02-17 07:12:16'),
-(16, 'Anggota DPRD Provinsi', '2020-02-17 07:12:16'),
-(17, 'Mengurus Rumah Tangga', '2020-02-17 07:12:16'),
-(18, 'Belum/ Tidak Bekerja', '2020-02-17 07:12:16'),
-(19, 'Pelajar/ Mahasiswa', '2020-02-17 07:12:16'),
-(20, 'Dokter', '2020-02-17 07:12:16'),
-(21, 'Pengacara', '2020-02-17 07:12:16'),
-(22, 'Menteri', '2020-02-17 07:12:16'),
-(23, 'Presiden', '2020-02-17 07:12:16'),
-(24, 'Kepolisian RI', '2020-02-17 07:12:16'),
-(25, 'Pensiunan', '2020-02-17 07:12:16'),
-(26, 'Perdagangan', '2020-02-17 07:12:16'),
-(27, 'Peternak', '2020-02-17 07:12:16'),
-(28, 'Nelayan/ Perikanan', '2020-02-17 07:12:16'),
-(29, 'Industri', '2020-02-17 07:12:17'),
-(30, 'Konstruksi', '2020-02-17 07:12:17'),
-(31, 'Transportasi', '2020-02-17 07:12:17'),
-(32, 'Karyawan BUMN', '2020-02-17 07:12:17'),
-(33, 'Karyawan BUMD', '2020-02-17 07:12:17'),
-(34, 'Buruh Tani / Perkebunan', '2020-02-17 07:12:17'),
-(35, 'Buruh Nelayan / Perikanan', '2020-02-17 07:12:17'),
-(36, 'Buruh Peternakan', '2020-02-17 07:12:17'),
-(37, 'Pembantu Rumah Tangga', '2020-02-17 07:12:17'),
-(38, 'Tukang Cukur', '2020-02-17 07:12:17'),
-(39, 'Tukang Listrik', '2020-02-17 07:12:17'),
-(40, 'Tukang Batu', '2020-02-17 07:12:17'),
-(41, 'Tukang Kayu', '2020-02-17 07:12:17'),
-(42, 'Tukang Sol Sepatu', '2020-02-17 07:12:18'),
-(43, 'Tukang Las / Pandai Besi', '2020-02-17 07:12:18'),
-(44, 'Tukang Jahit', '2020-02-17 07:12:18'),
-(45, 'Tukang Gigi', '2020-02-17 07:12:18'),
-(46, 'Penata Rias', '2020-02-17 07:12:18'),
-(47, 'Penata Busana', '2020-02-17 07:12:18'),
-(48, 'Penata Rambut', '2020-02-17 07:12:19'),
-(49, 'Mekanik', '2020-02-17 07:12:19'),
-(50, 'Seniman', '2020-02-17 07:12:19'),
-(51, 'Tabib', '2020-02-17 07:16:38'),
-(52, 'Paraji', '2020-02-17 07:16:38'),
-(53, 'Perancang Busana', '2020-02-17 07:16:38'),
-(54, 'Penterjemah', '2020-02-17 07:16:38'),
-(55, 'Imam Masjid', '2020-02-17 07:16:38'),
-(56, 'Pendeta', '2020-02-17 07:16:38'),
-(57, 'Pastur', '2020-02-17 07:16:38'),
-(58, 'Wartawan', '2020-02-17 07:16:38'),
-(59, 'Ustadz / Mubaligh', '2020-02-17 07:16:39'),
-(60, 'Juru Masak', '2020-02-17 07:16:39'),
-(61, 'Promotor Acara', '2020-02-17 07:16:39'),
-(62, 'Anggota BPK', '2020-02-17 07:16:39'),
-(63, 'Anggota Mahkamah Konstitusi', '2020-02-17 07:16:39'),
-(64, 'Dosen', '2020-02-17 07:16:39'),
-(65, 'Pilot', '2020-02-17 07:16:39'),
-(66, 'Notaris', '2020-02-17 07:16:39'),
-(67, 'Arsitek', '2020-02-17 07:16:39'),
-(68, 'Akuntan', '2020-02-17 07:16:40'),
-(69, 'Konsultan', '2020-02-17 07:16:40'),
-(70, 'Perawat', '2020-02-17 07:16:40'),
-(71, 'Apoteker', '2020-02-17 07:16:40'),
-(72, 'Psikiater / Psikolog', '2020-02-17 07:16:40'),
-(73, 'Penyiar Televisi', '2020-02-17 07:16:40'),
-(74, 'Penyiar Radio', '2020-02-17 07:16:40'),
-(75, 'Pelaut', '2020-02-17 07:16:40'),
-(76, 'Peneliti', '2020-02-17 07:16:40'),
-(77, 'Sopir', '2020-02-17 07:16:40'),
-(78, 'Pialang', '2020-02-17 07:16:40'),
-(79, 'Paranormal', '2020-02-17 07:16:40'),
-(80, 'Biarawati', '2020-02-17 07:16:41'),
-(81, 'Wiraswasta', '2020-02-17 07:16:41'),
-(82, 'Pekerjaan Lainnya', '2020-02-17 07:16:41'),
-(83, 'Duta Besar', '2020-02-17 07:16:41'),
-(84, 'Karyawan Honorer', '2020-02-17 07:16:41'),
-(85, 'Bidan', '2020-02-17 07:16:41'),
-(86, 'Anggota Kabinet / Kementerian', '2020-02-17 07:16:41'),
-(87, 'Gubernur', '2020-02-17 07:16:41'),
-(88, 'Wakil Bupati', '2020-02-17 07:16:42'),
-(89, 'Wakil Gubernur', '2020-02-17 07:16:42'),
-(90, 'Wakil Presiden', '2020-02-17 07:16:42'),
-(91, 'Wakil Walikota', '2020-02-17 07:16:42'),
-(92, 'Walikota', '2020-02-17 07:16:42');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `jakpwt_ref_pendidikan`
---
-
-CREATE TABLE `jakpwt_ref_pendidikan` (
-  `dikIdPendidikan` int(11) NOT NULL,
-  `dikPendidikan` varchar(500) DEFAULT NULL,
-  `dikLastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data untuk tabel `jakpwt_ref_pendidikan`
---
-
-INSERT INTO `jakpwt_ref_pendidikan` (`dikIdPendidikan`, `dikPendidikan`, `dikLastUpdate`) VALUES
-(1, 'Tidak / Belum Sekolah', '2020-02-17 07:06:28'),
-(2, 'Tamat SD / Sederajat', '2020-02-17 07:06:32'),
-(3, 'SLTP / Sederajat', '2020-02-17 07:06:36'),
-(4, 'SLTA / Sederajat', '2020-02-17 07:06:40'),
-(5, 'Diploma I / II', '2020-02-17 07:06:44'),
-(6, 'Diploma IV / Strata I\r\n', '2020-02-17 07:06:47'),
-(7, 'Belum Tamat SD / Sederajat\r\n', '2020-02-17 07:06:51'),
-(8, 'Akademi / Diploma III / Sarjana Muda', '2020-02-17 07:06:55'),
-(9, 'Strata II', '2020-02-17 07:06:58'),
-(10, 'Strata III', '2020-02-17 07:07:01');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `jakpwt_ref_wilayah`
---
-
+DROP TABLE IF EXISTS `jakpwt_ref_wilayah`;
 CREATE TABLE `jakpwt_ref_wilayah` (
-  `wilIdWilayah` int(11) NOT NULL,
-  `wilNama` varchar(500) DEFAULT NULL,
-  `wilLastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `wilIdWilayah` int NOT NULL AUTO_INCREMENT,
+  `wilNama` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `wilLastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`wilIdWilayah`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 
---
--- Dumping data untuk tabel `jakpwt_ref_wilayah`
---
+INSERT INTO `default_token` (`tokenId`, `token`) VALUES
+(1, '122025');
+
+INSERT INTO `default_user` (`userId`, `realname`, `username`, `password`, `foto`) VALUES
+(1, 'Super Admin', 'superadmin', '5f4dcc3b5aa765d61d8327deb882cf99', NULL);
 
 INSERT INTO `jakpwt_ref_wilayah` (`wilIdWilayah`, `wilNama`, `wilLastUpdate`) VALUES
-(1, 'Ajibarang', '2020-02-27 11:06:39'),
-(2, 'Banyumas', '2020-02-27 11:06:54'),
-(3, 'Purwokerto', '2020-02-27 13:20:52'),
-(4, 'Rantau', '2020-02-27 13:20:55'),
-(5, 'Wangon', '2020-02-27 13:21:03');
+(1, 'Ajibarang', '2020-02-27 18:06:39'),
+(2, 'Banyumas', '2020-02-27 18:06:54'),
+(3, 'Purwokerto', '2020-02-27 20:20:52'),
+(4, 'Rantau', '2020-02-27 20:20:55'),
+(5, 'Wangon', '2020-02-27 20:21:03');
 
---
--- Indexes for dumped tables
---
 
---
--- Indeks untuk tabel `default_token`
---
-ALTER TABLE `default_token`
-  ADD PRIMARY KEY (`tokenId`);
 
---
--- Indeks untuk tabel `default_user`
---
-ALTER TABLE `default_user`
-  ADD PRIMARY KEY (`userId`);
-
---
--- Indeks untuk tabel `jakpwt_anggota`
---
-ALTER TABLE `jakpwt_anggota`
-  ADD PRIMARY KEY (`agtId`),
-  ADD KEY `agtIdWilayah` (`agtIdWilayah`),
-  ADD KEY `agtIdPendidikan` (`agtIdPendidikan`),
-  ADD KEY `agtIdPekerjaan` (`agtIdPekerjaan`);
-
---
--- Indeks untuk tabel `jakpwt_kas`
---
-ALTER TABLE `jakpwt_kas`
-  ADD PRIMARY KEY (`kasId`);
-
---
--- Indeks untuk tabel `jakpwt_ref_pekerjaan`
---
-ALTER TABLE `jakpwt_ref_pekerjaan`
-  ADD PRIMARY KEY (`pkjIdPekerjaan`);
-
---
--- Indeks untuk tabel `jakpwt_ref_pendidikan`
---
-ALTER TABLE `jakpwt_ref_pendidikan`
-  ADD PRIMARY KEY (`dikIdPendidikan`) USING BTREE;
-
---
--- Indeks untuk tabel `jakpwt_ref_wilayah`
---
-ALTER TABLE `jakpwt_ref_wilayah`
-  ADD PRIMARY KEY (`wilIdWilayah`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `default_token`
---
-ALTER TABLE `default_token`
-  MODIFY `tokenId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `default_user`
---
-ALTER TABLE `default_user`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT untuk tabel `jakpwt_anggota`
---
-ALTER TABLE `jakpwt_anggota`
-  MODIFY `agtId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `jakpwt_kas`
---
-ALTER TABLE `jakpwt_kas`
-  MODIFY `kasId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `jakpwt_ref_wilayah`
---
-ALTER TABLE `jakpwt_ref_wilayah`
-  MODIFY `wilIdWilayah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
