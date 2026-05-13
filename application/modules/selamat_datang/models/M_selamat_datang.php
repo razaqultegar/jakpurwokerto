a9c78c39090c4baec5df60264685a0beacdbfde2
Razaqul Tegar <myrazaqul@gmail.com>
2026-05-13 15:34:43 +0700

    init: laravel 13

--- application/modules/selamat_datang/models/M_selamat_datang.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class M_selamat_datang extends CI_Model {
-  public function validate($token) {
-    $sql = "SELECT * FROM default_token WHERE token='$token'";
-    $query = $this->db->query($sql);
-    return $query->row();
-  }
-}
