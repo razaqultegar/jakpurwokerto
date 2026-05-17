25e597d57f7f61dd30431c7d9c2f98891c9399d3
Razaqul Tegar <myrazaqul@gmail.com>
2026-05-17 21:56:53 +0700

    feat: add default layouts

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,22 @@
+<div class="px-4">
+    <div class="border-b py-4 text-center text-sm text-onyx">Berdiri sejak 2013, Kitabisa memiliki izin Pengumpulan Uang dan Barang dari Kemensos. Kitabisa rutin diaudit dengan status Wajar Tanpa Pengecualian (WTP).</div>
+    <div class="pt-6">
+        <div class="mb-8 text-center text-sm text-onyx">
+            <a href="/tentang-kami" class="text-onyx" target="" rel="">Tentang Kitabisa</a> |
+            <a href="/syarat-ketentuan" class="text-onyx" target="_blank" rel="noopener noreferrer">Syarat &amp; Ketentuan</a> |
+            <a href="/pusat-bantuan" class="text-onyx" target="" rel="">Pusat Bantuan</a>
+        </div>
+        <div class="mb-8 grid grid-cols-3 gap-4 justify-items-center px-[10%]">
+            <a href="https://twitter.com/jakpurwokerto" class="flex h-8 w-8 items-center justify-center rounded-full bg-mercury" target="_blank" rel="noopener noreferrer">
+                <img class="h-5 w-6" src="https://assets.kitabisa.cc/images/icons/footer/icon-socmed-twitter.svg" alt="twitter" width="12" height="10" decoding="auto" fetchpriority="low">
+            </a>
+            <a href="https://www.instagram.com/jakpurwokerto/" class="flex h-8 w-8 items-center justify-center rounded-full bg-mercury" target="_blank" rel="noopener noreferrer">
+                <img src="https://assets.kitabisa.cc/images/icons/footer/icon-socmed-instagram.svg" class="h-5 w-6" alt="instagram" width="12" height="10" decoding="auto" fetchpriority="low">
+            </a>
+            <a data-testid="homepage-link-youtube" class="flex h-8 w-8 items-center justify-center rounded-full bg-mercury" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/user/jakpurwokerto">
+                <img class="h-5 w-6" src="https://assets.kitabisa.cc/images/icons/footer/icon-socmed-youtube.svg" alt="youtube" width="12" height="10" decoding="auto" fetchpriority="low">
+            </a>
+        </div>
+        <span class="mb-7 inline-block w-full text-center text-sm text-onyx">Copyright © {{ date('Y') }} {{ config('app.name') }}. All Rights Reserved</span>
+    </div>
+</div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=0">
+        <title>@yield('title') | {{ config('app.name') }}</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap">
+        @vite(['resources/assets/css/app.css', 'resources/assets/js/app.js'])
+    </head>
+    <body>
+        <main class="mx-auto my-0 min-h-full max-w-screen-sm">
+            <div class="mx-auto my-0 min-h-screen max-w-480 overflow-x-hidden bg-white pb-16.5">
+                @yield('content')
+                <x-footer />
+            </div>
+        </main>
+    </body>
+</html>
