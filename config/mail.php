69639f1c1e731d9964f8518f6197a65d32a3b50d
Razaqul Tegar <myrazaqul@gmail.com>
2026-05-19 20:45:30 +0700

    feat: add email integrations

--- config/mail.php
+++ config/mail.php
@@ -38,6 +38,7 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
